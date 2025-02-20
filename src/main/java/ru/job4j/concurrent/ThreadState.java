370e532f8be9f77cd0e8f0c7d6c1af43bb91b914
kostas-111 <galkink370@gmail.com>
2025-02-20 23:43:11 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+/*
+Чтобы получить состояние нити можно воспользоваться методом - getState().
+Этот метод возвращает перечисления Thread.State.
+Тело цикла while выполняется произвольное количество раз.
+За управление нитями в Java отвечает планировщик задач.
+Он решает, сколько времени отвести на выполнение одной задачи.
+ */
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
+            System.out.println("Работа завершена");
+        }
+    }
+}
