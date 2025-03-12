6c43048e19a9bbcf45af6c0379bb6f36bfd8c6e8
kostas-111 <galkink370@gmail.com>
2025-03-12 06:58:00 +0300

    fix: 8. Скачивание файла с ограничением. [#144271]
    - Избавиться от жесткой фиксации имени файла, в который качаем информацию
    - Добавить переменную в котороую будем суммировать общее число скачанных байт
    - Как только эта переменная достигла args, проверять за сколько времени это произошло и если быстрее 1с - ставить на паузу

--- src/main/java/ru/job4j/threads/Wget.java
+++ src/main/java/ru/job4j/threads/Wget.java
@@ -18,53 +18,66 @@ public class Wget implements Runnable {
 
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         long startAt = System.currentTimeMillis();
-        File file = new File("tmp.xml");
+        File file = new File(fileName);
         try (InputStream input = URI.create(url).toURL().openStream();
              FileOutputStream output = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long howManyBytesRead = 0;
+            long startTime = System.currentTimeMillis();
             while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
                 long downloadAt = System.nanoTime();
                 output.write(dataBuffer, 0, bytesRead);
                 long time = System.nanoTime() - downloadAt;
                 System.out.println("Read 1024 bytes : " + time + " nano.");
-                long currentSpeed = (1024 * 1000000) / time;
-                long pause = currentSpeed / speed;
-                if (speed != 6000) {
-                    System.out.printf("Calculated pause = %d ms.\n", pause);
-                    Thread.sleep(pause);
+                howManyBytesRead += bytesRead;
+                if (howManyBytesRead >= speed) {
+                    long timeToDownload = System.currentTimeMillis() - startTime;
+                    if (timeToDownload < 1000) {
+                        long currentSpeed = (1024 * 1000000) / time;
+                        long pause = currentSpeed / speed;
+                        System.out.printf("Calculated pause = %d ms.\n", pause);
+                        try {
+                            Thread.sleep(pause);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException();
+                        }
+                    }
                 }
+                System.out.println(Files.size(file.toPath()) + " bytes");
             }
-            System.out.println(Files.size(file.toPath()) + " bytes");
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
 
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Недостаточно аргументов. Пожалуйста, укажите URL и скорость.");
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Недостаточно аргументов. Пожалуйста, укажите URL, скорость и имя файла.");
         }
 
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
+        String fileName = args[2];
 
         if (speed <= 0) {
             throw new IllegalArgumentException("Скорость должна быть положительным целым числом.");
         }
 
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
