cc0bd352ca98c3ab0b0eb935d50356c600c7c4e4
galkinki <galkin@safe-line.ru>
2025-10-27 11:49:08 +0300

    feat: 2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/threads/pool/EmailNotification.java
@@ -0,0 +1,48 @@
+package ru.job4j.threads.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+  /*
+  Создаем пул нитей по количеству доступных процессоров
+   */
+  ExecutorService pool = Executors.newFixedThreadPool(
+      Runtime.getRuntime().availableProcessors());
+
+  public void emailTo(User user) {
+    String subject = String.format("Notification %s to email %s",
+        user.getName(), user.getEmail());
+    String body = String.format("Add a ew event to %s", user.getName());
+
+    /*
+    Через ExecutorService создаем задачу, которая будет создавать
+    данные для пользователя и передавать их в метод send
+     */
+    pool.submit(new Runnable() {
+      @Override
+      public void run() {
+        send(subject, body, user.getEmail());
+      }
+    });
+  }
+
+  /*
+  Закрываем пул и ждем пока все задачи завершатся
+   */
+  public void close() {
+    pool.shutdown();
+    while (!pool.isTerminated()) {
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void send(String subject, String body, String email) {
+  }
+
+}
---
+++ src/main/java/ru/job4j/threads/pool/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.threads.pool;
+
+public class User {
+
+  private String name;
+  private String email;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
